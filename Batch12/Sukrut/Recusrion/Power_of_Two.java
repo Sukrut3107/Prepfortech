1cef00afcfb181dd6661b6593da036fb9f5cb608
Sukrut Bilaskar <sukrut515@gmail.com>
2022-04-13 12:53:50 +0530

    Update Power_of_Two.java

--- Batch12/Sukrut/Recusrion/Power_of_Two.java
+++ Batch12/Sukrut/Recusrion/Power_of_Two.java
@@ -10,3 +10,14 @@ class Solution {
         return isPowerOfTwo(n/2);
     }
 }
+
+
+// Alternative Solution for the same problem is
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+         if(n<=0){
+            return false;
+        }
+        return (n & (n-1))==0;
+}
+}
